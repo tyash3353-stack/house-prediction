ba17ee80a82b7ca7e20d8c7f72b6b1660cb20d7c
tyash3353-stack <tyash3353@gmail.com>
2026-05-18 22:40:16 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 import streamlit as st
 import joblib
-import numpy as np
+import pandas as pd
 
 model = joblib.load("house_price_model.pkl")
 
 st.title("House Price Prediction")
 
-area = st.number_input("Enter Area")
-bedrooms = st.number_input("Enter Bedrooms")
+area = st.number_input("Area")
+bedrooms = st.number_input("Bedrooms")
+bathrooms = st.number_input("Bathrooms")
 
-if st.button("Predict Price"):
+if st.button("Predict"):
 
-    features = np.array([[area, bedrooms]])
+    input_data = pd.DataFrame({
+        'Area': [area],
+        'Bedrooms': [bedrooms],
+        'Bathrooms': [bathrooms]
+    })
 
-    prediction = model.predict(features)
+    prediction = model.predict(input_data)
 
     st.success(f"Predicted Price: {prediction[0]}")
